cb163556cd5eb589e46f63dc8531140bdbf046ce
caunt <gpg@caunt.world>
2025-10-21 06:51:15 +0300

    fix(network): ✨ update default upstream host
    Updated the default `upstreamHost` to connect to the Hay Day game server (`game.haydaygame.com`) instead of the Brawl Stars game server (`34.227.74.196`). This change ensures the application defaults to the Hay Day server when no upstream host argument is provided.

--- Playground/Program.cs
+++ Playground/Program.cs
@@ -3,7 +3,7 @@ using System.Buffers.Binary;
 using System.Net;
 using System.Net.Sockets;
 
-var upstreamHost = args.Length > 0 ? args[0] : "34.227.74.196"; // "game.brawlstarsgame.com";
+var upstreamHost = args.Length > 0 ? args[0] : "game.haydaygame.com";
 var listenPort = args.Length > 1 && int.TryParse(args[1], out var lp) ? lp : 9339;
 var upstreamPort = args.Length > 2 && int.TryParse(args[2], out var up) ? up : 9339;
 
