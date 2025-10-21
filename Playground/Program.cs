cdd67fe93a8fe6e82a0f290dc679ab0831989aee
caunt <gpg@caunt.world>
2025-10-21 06:22:58 +0300

    feat(init): first commit

---
+++ Playground/Program.cs
@@ -0,0 +1,170 @@
+﻿using Playground;
+using System.Buffers.Binary;
+using System.Net;
+using System.Net.Sockets;
+
+var upstreamHost = args.Length > 0 ? args[0] : "34.227.74.196"; // "game.brawlstarsgame.com";
+var listenPort = args.Length > 1 && int.TryParse(args[1], out var lp) ? lp : 9339;
+var upstreamPort = args.Length > 2 && int.TryParse(args[2], out var up) ? up : 9339;
+
+var listener = new TcpListener(IPAddress.Any, listenPort);
+listener.Start();
+Console.WriteLine($"[{DateTime.Now:T}] Listening on 0.0.0.0:{listenPort}, upstream {upstreamHost}:{upstreamPort}");
+
+while (true)
+{
+    var client = await listener.AcceptTcpClientAsync();
+    _ = HandleClientAsync(client, upstreamHost, upstreamPort);
+}
+
+static async Task HandleClientAsync(TcpClient client, string upstreamHost, int upstreamPort)
+{
+    using var clientConn = client;
+    var remote = clientConn.Client.RemoteEndPoint?.ToString() ?? "client";
+    Console.WriteLine($"[{DateTime.Now:T}] Incoming connection from {remote}");
+
+    using var upstream = new TcpClient();
+    await upstream.ConnectAsync(upstreamHost, upstreamPort);
+
+    using var c2s = clientConn.GetStream();
+    using var s2c = upstream.GetStream();
+
+    try
+    {
+        await Task.WhenAll(PumpAsync(c2s, s2c, Direction.Serverbound), PumpAsync(s2c, c2s, Direction.Clientbound));
+    }
+    catch (Exception exception)
+    {
+        Console.WriteLine($"[{DateTime.Now:T}] {remote} closed: {exception.Message}");
+    }
+}
+
+static async Task PumpAsync(NetworkStream source, NetworkStream destination, Direction direction)
+{
+    var header = new byte[7];
+
+    while (true)
+    {
+        await source.ReadExactlyAsync(header);
+
+        var span = header.AsSpan();
+
+        var id = BinaryPrimitives.ReadUInt16BigEndian(span[0..2]);
+        var length = (span[2] << 16) | (span[3] << 8) | span[4];
+        var version = BinaryPrimitives.ReadUInt16BigEndian(span[5..7]);
+
+        await destination.WriteAsync(header);
+        var payload = new byte[length];
+
+        if (length > 0)
+        {
+            await source.ReadExactlyAsync(payload);
+            await destination.WriteAsync(payload);
+        }
+
+        try
+        {
+            using var stream = new ScStream(payload);
+            await PacketSent(direction, id, version, stream);
+        }
+        catch (Exception exception)
+        {
+            Console.WriteLine($"[{DateTime.Now:T}] Error handling {direction} packet id={id} length={length} version={version}:\n{exception}");
+        }
+    }
+}
+
+static async Task PacketSent(Direction direction, ushort id, ushort version, ScStream stream)
+{
+    var prefix = $"[{DateTime.Now:T}][{direction.ToString().ToLower()},version={PadLeft(version, 4)},id={PadLeft(id, 5)}]";
+
+    switch (direction, id)
+    {
+        case (Direction.Serverbound, 10100):
+            var protocolVersion = stream.ReadInt32();
+            var keyVersion = stream.ReadInt32();
+
+            var clientVersionMajor = stream.ReadInt32();
+            var clientVersionMinor = stream.ReadInt32();
+            var clientVersionPatch = stream.ReadInt32();
+
+            var fingerprintSha1 = stream.ReadString();
+
+            var flag1 = stream.ReadInt32();
+            var flag2 = stream.ReadInt32();
+            Console.WriteLine($"{prefix} Hello from Client" +
+                $"\n\tprotocolVersion={protocolVersion}" +
+                $"\n\tkeyVersion={keyVersion}" +
+                $"\n\tclientVersion={clientVersionMajor}.{clientVersionMinor}.{clientVersionPatch}" +
+                $"\n\tfingerprintSha1={fingerprintSha1}" +
+                $"\n\tflags=[{flag1}, {flag2}]");
+            break;
+        case (Direction.Clientbound, 20103):
+            // 8 - UpdateRequired
+            var errorCode = stream.ReadInt32();
+            var resourceFingerprintData = stream.ReadString();
+            var redirectDomain = stream.ReadString();
+            var contentURL = stream.ReadString();
+            var updateURL = stream.ReadString();
+            var reason = stream.ReadString();
+            var secondsUntilMaintenanceEnd = stream.ReadInt32();
+            var unknown1 = stream.ReadByte();
+            var unknown2 = stream.ReadString();
+            var unknown3 = stream.ReadString();
+            var unknown4 = stream.ReadInt32();
+            var unknown5 = stream.ReadInt32();
+            var unknown6 = stream.ReadString();
+            var unknown7 = stream.ReadString();
+
+            var unknown8 = new int[BinaryPrimitives.ReverseEndianness(stream.ReadUInt16())];
+            for (var i = 0; i < unknown8.Length; i++)
+                unknown8[i] = stream.ReadInt32();
+
+            var unknown9 = stream.ReadByte();
+
+            Console.WriteLine($"{prefix} LoginFailed from server" +
+                $"\n\terrorCode={errorCode}" +
+                $"\n\tresourceFingerprintData={resourceFingerprintData}" +
+                $"\n\tredirectDomain={redirectDomain}" +
+                $"\n\tcontentURL={contentURL}" +
+                $"\n\tupdateURL={updateURL}" +
+                $"\n\treason={reason}" +
+                $"\n\tsecondsUntilMaintenanceEnd={secondsUntilMaintenanceEnd}" +
+                $"\n\tunknown1={unknown1}" +
+                $"\n\tunknown2={unknown2}" +
+                $"\n\tunknown3={unknown3}" +
+                $"\n\tunknown4={unknown4}" +
+                $"\n\tunknown5={unknown5}" +
+                $"\n\tunknown6={unknown6}" +
+                $"\n\tunknown7={unknown7}" +
+                $"\n\tunknown8=[{string.Join(", ", unknown8)}]" +
+                $"\n\tunknown9={unknown9}");
+            break;
+        default:
+            const int maxWidth = 64;
+
+            var data = stream.ReadToEnd();
+            var sliced = data.Length > maxWidth;
+            var placeholder = Convert.ToHexString(data[..Math.Min(maxWidth, data.Length)]);
+
+            if (sliced)
+                placeholder += "...";
+
+            Console.WriteLine($"{prefix} length={PadLeft(stream.Length, 5)} => {placeholder}");
+            break;
+    }
+
+    static string? PadLeft<T>(T value, int width, char @char = '.') where T : struct
+    {
+        return value.ToString()?.PadLeft(width, @char);
+    }
+}
+
+// https://github.com/ReversedCell/ScDocumentation/wiki/Encryption-Setup
+// https://github.com/ReversedCell/ScDocumentation/wiki/Protocol
+
+enum Direction
+{
+    Clientbound,
+    Serverbound
+}
---
+++ Playground/ScStream.cs
@@ -0,0 +1,116 @@
+﻿using System.Buffers.Binary;
+using System.Text;
+
+namespace Playground;
+
+public class ScStream(byte[] buffer) : IDisposable
+{
+    public int Position { get; private set; } = 0;
+    public int Length => buffer.Length;
+    public Span<byte> Span => buffer;
+
+    protected int _booleanOffset;
+    protected byte _booleanAdditionalValue;
+
+    public byte ReadByte()
+    {
+        ResetBoolean();
+
+        if (Position + 1 > Length)
+            throw new EndOfStreamException();
+
+        return buffer[Position++];
+    }
+
+    public Span<byte> ReadBytes(int count)
+    {
+        ResetBoolean();
+
+        if (Position + count > Length)
+            throw new EndOfStreamException();
+
+        var span = buffer.AsSpan(Position, count);
+        Position += count;
+
+        return span;
+    }
+
+    public Span<byte> ReadToEnd()
+    {
+        return ReadBytes(Length - Position);
+    }
+
+    public bool ReadBoolean()
+    {
+        if (_booleanOffset == 0)
+            _booleanAdditionalValue = ReadByte();
+
+        var value = ((_booleanAdditionalValue >> _booleanOffset) & 1) != 0;
+        _booleanOffset = (_booleanOffset + 1) & 7;
+
+        return value;
+    }
+
+    public ushort ReadUInt16()
+    {
+        return BinaryPrimitives.ReadUInt16BigEndian(ReadBytes(2));
+    }
+
+    public int ReadInt32()
+    {
+        return BinaryPrimitives.ReadInt32BigEndian(ReadBytes(4));
+    }
+
+    public string? ReadString()
+    {
+        var length = ReadInt32();
+
+        if (length < 0)
+            return null;
+
+        return Encoding.UTF8.GetString(ReadBytes(length));
+    }
+
+    public int ReadVarInt()
+    {
+        var firstByte = ReadByte();
+        var isNegative = (firstByte & 0x40) != 0;
+        var accumulator = firstByte & 0x3FL;
+        var consumedBitWidth = 6;
+
+        var currentByte = firstByte;
+        while ((currentByte & 0x80) != 0 && consumedBitWidth < 64)
+        {
+            currentByte = ReadByte();
+            accumulator |= (long)(currentByte & 0x7F) << consumedBitWidth;
+            consumedBitWidth += 7;
+        }
+
+        if (isNegative)
+        {
+            var twoComplementBase = 1L << consumedBitWidth;
+            accumulator -= twoComplementBase;
+        }
+
+        return (int)accumulator;
+    }
+
+    private void ResetBoolean()
+    {
+        if (_booleanOffset <= 0)
+            return;
+
+        _booleanOffset = 0;
+        _booleanAdditionalValue = 0;
+    }
+
+    public void Dispose()
+    {
+        GC.SuppressFinalize(this);
+
+        if (Position >= Length)
+            return;
+
+        throw new InvalidOperationException($"Read operation incomplete: {Position} / {Length} bytes read.\n{BitConverter.ToString(buffer).Replace('-', ' ').Insert(Position * 3, "> ")}");
+    }
+}
