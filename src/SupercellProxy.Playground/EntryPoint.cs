335f7c7f2ac9b262a4f16a7ad03f6e542347d975
caunt <gpg@caunt.world>
2026-02-28 19:41:14 +0200

    refactor(entrypoint): ♻️ simplify to client-only mode
    Refactored entry point to run only the client, commenting out server and proxy initialization and related variables. Removed unused using directives to streamline code and focus on client-side functionality.

--- src/SupercellProxy.Playground/EntryPoint.cs
+++ src/SupercellProxy.Playground/EntryPoint.cs
@@ -1,16 +1,16 @@
 ﻿using SupercellProxy.Playground.Network.Sides;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
 
-var upstreamHost = args.Length > 0 ? args[0] : "game.haydaygame.com"; // "game.squadbustersgame.com"; //
+var upstreamHost = args.Length > 0 ? args[0] : "game.haydaygame.com";
 var upstreamPort = args.Length > 1 && int.TryParse(args[1], out var up) ? up : 9339;
-var listenAddress = args.Length > 2 ? args[2] : "0.0.0.0";
-var listenPort = args.Length > 3 && int.TryParse(args[3], out var lp) ? lp : 9339;
 
 var client = new Client(upstreamHost, upstreamPort);
-var server = new Server(listenAddress, listenPort);
-var proxy = new Proxy(upstreamHost, upstreamPort, listenAddress, listenPort);
-
 await client.RunAsync();
+
+// var listenAddress = args.Length > 2 ? args[2] : "0.0.0.0";
+// var listenPort = args.Length > 3 && int.TryParse(args[3], out var lp) ? lp : 9339;
+// 
+// var server = new Server(listenAddress, listenPort);
 // await server.RunAsync();
+//
+// var proxy = new Proxy(upstreamHost, upstreamPort, listenAddress, listenPort);
 // await proxy.RunAsync();
