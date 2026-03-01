0aa59975306b8d06270921133983a30a213ccd3a
caunt <gpg@caunt.world>
2026-03-02 01:47:32 +0200

    docs(network): 📝 clarify proxy key usage in comments
    Updated comments to explicitly list the standard proxy private key, public key, and encoded public key values for improved clarity. Added instructions for patching the app's memory on Android to replace the server public key with the standard proxy public key using PublicKeyCodec.Encode().

--- src/SupercellProxy.Playground/Network/Sides/Proxy.cs
+++ src/SupercellProxy.Playground/Network/Sides/Proxy.cs
@@ -7,14 +7,17 @@ using System.Net.Sockets;
 
 namespace SupercellProxy.Playground.Network.Sides;
 
-// There are multiple ways to implement a proxy that can decrypt packets.
+// There are multiple ways to implement a proxy that can decrypt packets:
 // 1) Get the 'client private key' from the app and give it to the proxy (not sure but maybe 'snonce' needed as well).
 // 2) Replace the 'server public key' in the app with 'proxy public key', re-encrypt packets on the proxy.
+
 // CoCSharp.Proxy uses second approach - https://github.com/FICTURE7/CoCSharp/blob/d8602264fd185a9236197502eb40aa57019bf4be/src/CoCSharp.Proxy/MessageProcessorNaClProxy.cs#L99
 // They introduced a "standard" key pair for modded servers at Crypto8.StandardKeyPair:
-// standard(proxy) public key: 72F1A4A4C48E44DA0C42310F800E96624E6DC6A641A9D41C3B5039D8DFADC27E
-// standard(proxy) public key encoded: 5E2E00002929000047620000DA440000841800003CC400007400000029660000CDA90000A9B10000D4A000001CD40000A076000060E700006EFD0000EC27000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
-// If the goal is just to log unencrypted packets, maybe patch the app to log them directly.
+// standard proxy private key: 1891D401FADB51D25D3A9174D472A9F691A45B974285D47729C45C6538070D85
+// standard proxy public key: 72F1A4A4C48E44DA0C42310F800E96624E6DC6A641A9D41C3B5039D8DFADC27E
+// standard proxy public key encoded: 5E2E00002929000047620000DA440000841800003CC400007400000029660000CDA90000A9B10000D4A000001CD40000A076000060E700006EFD0000EC27000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000
+
+// We are also using second approach, by patching the app's memory to replace the server public key with the standard proxy public key encoded by PublicKeyCodec.Encode()
 
 // Android in-memory public key replacemet:
 // 1) Set up Wi-Fi Remote ADB Shell on rooted device
