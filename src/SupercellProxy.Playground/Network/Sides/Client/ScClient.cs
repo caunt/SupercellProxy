a37189956ad65e8708f4d47f132ae9b78533c219
caunt <gpg@caunt.world>
2026-03-24 19:55:57 +0200

    feat(network): ✨ improve login and asset handling
    Refactored login to support dynamic resource fingerprinting and asset downloading when outdated content is detected. Added processing and display of building goods from resources. Enhanced asset caching, error handling, and logging. Cleaned up code and improved naming consistency.

--- src/SupercellProxy.Playground/Network/Sides/Client/ScClient.cs
+++ src/SupercellProxy.Playground/Network/Sides/Client/ScClient.cs
@@ -2,33 +2,40 @@ using SupercellProxy.Playground.Exceptions;
 using SupercellProxy.Playground.Network.Messages.Clientbound;
 using SupercellProxy.Playground.Network.Messages.Serverbound;
 using SupercellProxy.Playground.Network.Sides.Configuration;
+using SupercellProxy.Playground.Resources;
 using SupercellProxy.Playground.Supercell;
-using System.Text.Json.Nodes;
+using System.Runtime.CompilerServices;
+using System.Text.Json;
 
 namespace SupercellProxy.Playground.Network.Sides;
 
 public partial class ScClient(ClientConfiguration configuration) : IAsyncDisposable
 {
-    private static readonly TimeSpan _keepAliveInterval = TimeSpan.FromSeconds(5);
+    private static readonly TimeSpan KeepAliveInterval = TimeSpan.FromSeconds(5);
+    private static readonly JsonSerializerOptions JsonSerializerOptions = new() { WriteIndented = true };
+
+    private readonly HttpClient _httpClient = new();
 
     public async Task RunAsync(CancellationToken cancellationToken = default)
     {
         try
         {
-            var loginOkMessage = await LoginAsync(cancellationToken);
+            var loginOkResult = await LoginAsync(cancellationToken);
 
-            Console.WriteLine(loginOkMessage);
+            Console.WriteLine(loginOkResult);
 
             var keepAliveTask = Task.Run(async () =>
             {
                 while (!cancellationToken.IsCancellationRequested)
                 {
-                    await Task.Delay(_keepAliveInterval);
+                    await Task.Delay(KeepAliveInterval);
 
                     var stream = await GetStreamAsync(cancellationToken);
                     await stream.WriteMessageAsync(new KeepAliveMessage(), cancellationToken);
                 }
             }, cancellationToken);
+
+            HandleGoods(loginOkResult.Resources);
 
             while (!keepAliveTask.IsCompleted)
                 await HandleIncomingMessageAsync(cancellationToken);
@@ -51,90 +58,178 @@ public partial class ScClient(ClientConfiguration configuration) : IAsyncDisposa
         GC.SuppressFinalize(this);
     }
 
-    private async Task<LoginOkMessage> LoginAsync(CancellationToken cancellationToken = default)
+    private static void HandleGoods(Resource[] resources)
+    {
+        const string ProcessingBuildingsFileName = "processing_buildings.csv";
+
+        var processingBuildingsResource = resources.FirstOrDefault(resource => resource.Fingerprint.File.EndsWith(ProcessingBuildingsFileName))
+            ?? throw new InvalidOperationException($"{ProcessingBuildingsFileName} not found in resources.");
+
+        if (!processingBuildingsResource.TryGetTable(out var processingBuildings))
+            throw new InvalidOperationException($"Failed to parse {ProcessingBuildingsFileName} from resources.");
+
+        for (var i = 0; i < processingBuildings.Entries.Count; i++)
+        {
+            var processingBuilding = processingBuildings.Entries[i];
+            var processingBuildingName = processingBuilding.BaseRow.First(field => field.Key.Equals("Name", StringComparison.OrdinalIgnoreCase)).Value;
+            var processingBuildingGoodsResource = resources.FirstOrDefault(resource => resource.Fingerprint.File.EndsWith($"{processingBuildingName}_goods.csv", StringComparison.OrdinalIgnoreCase));
+
+            if (processingBuildingGoodsResource is null)
+            {
+                Console.WriteLine($"[{i}] {processingBuildingName} has no goods.");
+                continue;
+            }
+
+            if (!processingBuildingGoodsResource.TryGetTable(out var goods))
+            {
+                Console.WriteLine($"[{i}] {processingBuildingName} can't parse goods.");
+                continue;
+            }
+
+            Console.WriteLine($"[{i}] {processingBuildingName} has {goods.Entries.Count} goods => {string.Join(", ", goods.Entries.Select(good => good.BaseRow.First(field => field.Key.Equals("Name", StringComparison.OrdinalIgnoreCase)).Value))}");
+        }
+    }
+
+    private async Task<LoginOkResult> LoginAsync(CancellationToken cancellationToken = default)
     {
         try
         {
             // 1.67.170 => be514e02b198d18287af1405089a0e72b849ac69
             // 1.67.175 => fdb648cea5e3494c3cafc32eca103331d85c5bfd
             // 1.69.89  => 0c95746ec8ced89978f4b9fded2fdbc95b3daf18
-            return await LoginAsync(fingerprintSha1: string.Empty, cancellationToken);
+            // Since then, we detect the fingerprint dynamically from the login failed message
+            return new LoginOkResult(await LoginCoreAsync(fingerprintSha1: string.Empty, cancellationToken));
         }
         catch (LoginException loginException) when (loginException.LoginFailedMessage.ErrorCode is LoginFailedMessage.Type.OutdatedContent)
         {
-            var node = JsonNode.Parse(loginException.LoginFailedMessage.ResourceFingerprintData);
-            var fingerprint = node?["sha"]?.GetValue<string>();
+            var fingerprint = loginException.LoginFailedMessage.ResourceFingerprint;
+            var resources = await GetAssetsAsync(fingerprint, loginException.LoginFailedMessage.AssetsUrlsFiltered, cancellationToken).ToArrayAsync(cancellationToken);
 
-            if (string.IsNullOrWhiteSpace(fingerprint))
+            if (string.IsNullOrWhiteSpace(fingerprint.Sha))
                 throw new InvalidOperationException($"Failed to parse fingerprint from login failed message:\n{loginException.LoginFailedMessage.ResourceFingerprintData}", loginException);
 
-            return await LoginAsync(fingerprint, cancellationToken);
+            return new LoginOkResult(await LoginCoreAsync(fingerprint.Sha, cancellationToken), resources);
+        }
+
+        async Task<LoginOkMessage> LoginCoreAsync(string fingerprintSha1, CancellationToken cancellationToken = default)
+        {
+            try
+            {
+                var stream = await GetStreamAsync(cancellationToken);
+
+                await stream.WriteMessageAsync(new ClientHelloMessage
+                {
+                    ProtocolVersion = configuration.Protocol.ProtocolVersion,
+                    KeyVersion = configuration.Protocol.KeyVersion,
+
+                    MajorVersion = configuration.Protocol.MajorVersion,
+                    MinorVersion = configuration.Protocol.MinorVersion,
+                    PatchVersion = configuration.Protocol.PatchVersion,
+
+                    FingerprintSha1 = fingerprintSha1,
+
+                    DeviceType = 1,
+                    AppStore = 1,
+                    Unknown1 = -1
+                }, cancellationToken);
+
+                var message = await stream.ReadMessageAsync(cancellationToken);
+                LoginException.ThrowIfFailed(message);
+
+                if (message is not ServerHelloMessage serverHello)
+                    throw new InvalidOperationException($"Expected {nameof(ServerHelloMessage)}, but received {message}.");
+
+                await stream.SetupEncryptionAsync(Side.Server, serverHello.SessionKey, cancellationToken);
+
+                await stream.WriteMessageAsync(new LoginMessage
+                {
+                    AccountId = LogicLong.Empty,
+                    PassToken = null,
+                    ResourceSha = fingerprintSha1,
+                    LoginVersion = 1119325,
+                    UdId = "",
+                    OpenUdId = "",
+                    MacAddress = "",
+                    DeviceModel = "iPad9,1",
+                    AdId = "",
+                    IsAdTracking = false,
+                    OsVersion = "18.2",
+                    Locale = "",
+                    Idfv = "",
+                    PreferredLanguage = "",
+                    ScidString = "",
+                    Unknown0 = true,
+                    ScIdToken = ""
+                }, cancellationToken);
+
+                message = await stream.ReadMessageAsync(cancellationToken);
+                LoginException.ThrowIfFailed(message);
+
+                if (message is not LoginOkMessage loginOkMessage)
+                    throw new InvalidOperationException($"Expected {nameof(LoginOkMessage)}, but received {message}.");
+
+                return loginOkMessage;
+            }
+            catch
+            {
+                await DisconnectAsync(cancellationToken);
+                throw;
+            }
+        }
+
+        async IAsyncEnumerable<Resource> GetAssetsAsync(ResourceFingerprint fingerprint, IEnumerable<string> downloadUrls, [EnumeratorCancellation] CancellationToken cancellationToken = default)
+        {
+            var assetsDirectory = Directory.CreateDirectory(Path.Combine(AppContext.BaseDirectory, "Assets", fingerprint.Version, fingerprint.Sha));
+
+            foreach (var file in fingerprint.Files)
+            {
+                var filePath = Path.Combine(assetsDirectory.FullName, file.File);
+
+                if (File.Exists(filePath))
+                {
+                    yield return new Resource(file, await File.ReadAllBytesAsync(filePath, cancellationToken));
+                    continue;
+                }
+
+                if (Path.GetDirectoryName(filePath) is { } directoryName)
+                    _ = Directory.CreateDirectory(directoryName);
+
+                var downloaded = false;
+
+                foreach (var downloadUrl in downloadUrls)
+                {
+                    try
+                    {
+                        var response = await _httpClient.GetAsync($"{downloadUrl.Trim('/')}/{fingerprint.Sha.Trim('/')}/{file.File.Trim('/')}", cancellationToken);
+                        response.EnsureSuccessStatusCode();
+
+                        await using var fileStream = File.Create(filePath);
+                        await using var httpStream = await response.Content.ReadAsStreamAsync(cancellationToken);
+                        await httpStream.CopyToAsync(fileStream, cancellationToken);
+
+                        downloaded = true;
+                    }
+                    catch (Exception exception) when (exception is HttpRequestException || exception is IOException)
+                    {
+                        Console.WriteLine($"Failed to download {file.File} from {downloadUrl}: {exception.Message}");
+                    }
+
+                    if (!downloaded)
+                        continue;
+
+                    Console.WriteLine($"Downloaded {file.File} from {downloadUrl}");
+                    yield return new Resource(file, await File.ReadAllBytesAsync(filePath, cancellationToken));
+                    break;
+                }
+            }
         }
     }
 
-    private async Task<LoginOkMessage> LoginAsync(string fingerprintSha1, CancellationToken cancellationToken = default)
+    private record LoginOkResult(LoginOkMessage LoginOkMessage, Resource[] Resources)
     {
-        try
+        public LoginOkResult(LoginOkMessage loginOkMessage) : this(loginOkMessage, [])
         {
-            var stream = await GetStreamAsync(cancellationToken);
-
-            await stream.WriteMessageAsync(new ClientHelloMessage
-            {
-                ProtocolVersion = configuration.Protocol.ProtocolVersion,
-                KeyVersion = configuration.Protocol.KeyVersion,
-
-                MajorVersion = configuration.Protocol.MajorVersion,
-                MinorVersion = configuration.Protocol.MinorVersion,
-                PatchVersion = configuration.Protocol.PatchVersion,
-
-                FingerprintSha1 = fingerprintSha1,
-
-                DeviceType = 1,
-                AppStore = 1,
-                Unknown1 = -1
-            }, cancellationToken);
-
-            var message = await stream.ReadMessageAsync(cancellationToken);
-            LoginException.ThrowIfFailed(message);
-
-            if (message is not ServerHelloMessage serverHello)
-                throw new InvalidOperationException($"Expected {nameof(ServerHelloMessage)}, but received {message}.");
-
-            await stream.SetupEncryptionAsync(Side.Server, serverHello.SessionKey, cancellationToken);
-
-            await stream.WriteMessageAsync(new LoginMessage
-            {
-                AccountId = LogicLong.Empty,
-                PassToken = null,
-                ResourceSha = fingerprintSha1,
-                LoginVersion = 1119325,
-                UdId = "",
-                OpenUdId = "",
-                MacAddress = "",
-                DeviceModel = "iPad9,1",
-                AdId = "",
-                IsAdTracking = false,
-                OsVersion = "18.2",
-                Locale = "",
-                Idfv = "",
-                PreferredLanguage = "",
-                ScidString = "",
-                Unknown0 = true,
-                ScIdToken = ""
-            }, cancellationToken);
-
-            message = await stream.ReadMessageAsync(cancellationToken);
-            LoginException.ThrowIfFailed(message);
-
-            if (message is not LoginOkMessage loginOkMessage)
-                throw new InvalidOperationException($"Expected {nameof(LoginOkMessage)}, but received {message}.");
-
-            return loginOkMessage;
-        }
-        catch
-        {
-            await DisconnectAsync(cancellationToken);
-            throw;
+            // Empty
         }
     }
 }
